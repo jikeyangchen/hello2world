8c763993ce82c9a5efc0ccc173d106a13f40f6a8
yangchen <1825829099@qq.com>
2018-05-06 00:33:50 +0800

    Initial commit

---
+++ app/src/main/java/com/example/my94my/helloworld/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.my94my.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
